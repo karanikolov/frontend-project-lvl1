39e14ea275b6de27a94d3f1ff4c203b91e3d9078
Nikolay Karanikolov <karanikolov333@gmail.com>
2021-03-21 17:46:13 +0200

    last

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,6 @@
 #!/usr/bin/env node
 
 console.log("Welcome to the Brain Games!")
+import readlineSync from 'readline-sync';
+var userName = readlineSync.question('May I have your name? ');
+console.log('Hi ' + userName + '!');
